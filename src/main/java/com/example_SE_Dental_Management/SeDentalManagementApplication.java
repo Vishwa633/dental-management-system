93473326b71e19b19a13c81c6b04e31905272da9
Vishwa633 <vishwanethsara633@gmail.com>
2025-11-22 13:52:30 +0530

    initial commit

---
+++ src/main/java/com/example_SE_Dental_Management/SeDentalManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example_SE_Dental_Management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeDentalManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeDentalManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example_SE_Dental_Management/SeDentalManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example_SE_Dental_Management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeDentalManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
